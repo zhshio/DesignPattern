faa3dabedf5b24c26d9a1fc358b4062594f97ace
OhtoAi1024 <muzhi567@qq.com>
2024-03-20 19:23:41 +0800

    feature(creation): 单例模式课堂示例与练习

---
+++ creation/src/main/java/edu/zhshio/singleton/sample/multthreadsafe/PrintSpoolerSingleton.java
@@ -0,0 +1,13 @@
+package edu.zhshio.singleton.sample.multthreadsafe;/**
+*@Auther: 张帅
+*@Date: 2024/3/11 - 03 - 11 - 15:35 
+*@Description: edu.zhshio.singleton.sample.multthreadsafe
+*@version: 1.0 
+*/ /**
+ * @description: 
+ * @author: zs
+ * @time: 2024/3/11 15:35
+ */
+ 
+public class PrintSpoolerSingleton {
+}
